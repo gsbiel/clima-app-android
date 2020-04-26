2d1aaef5be87352b256942b4eaa6821ccf618cdc
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-04-26 03:26:20 -0300

    implement static layout of main screen

--- app/src/main/java/com/example/climaapp/MainActivity.java
+++ app/src/main/java/com/example/climaapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.climaapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
         setContentView(R.layout.activity_main);
     }
 }
