d326d4e17484bb5bf9771ba33dcc1025e2757765
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-07-06 21:09:34 -0300

    create Room Database class and DAO interface

--- app/src/main/java/com/example/climaapp/database/WeatherDatabase.kt
+++ app/src/main/java/com/example/climaapp/database/WeatherDatabase.kt
@@ -1,2 +1,32 @@
 package com.example.climaapp.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [WeatherData::class], version = 1, exportSchema = false)
+abstract class WeatherDatabase: RoomDatabase() {
+    companion object {
+        @Volatile
+        private var INSTANCE: WeatherDatabase? = null
+
+        fun getInstance(context: Context): WeatherDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+                if(instance == null){
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            WeatherDatabase::class.java,
+                            "weather_database"
+                    )
+                            .fallbackToDestructiveMigration()
+                            .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/example/climaapp/database/WeatherDatabaseDao.kt
+++ app/src/main/java/com/example/climaapp/database/WeatherDatabaseDao.kt
@@ -1,2 +1,15 @@
 package com.example.climaapp.database
 
+import androidx.room.*
+
+@Dao
+interface WeatherDatabaseDao{
+    @Update
+    fun update(weatherData: WeatherData)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(weatherData: WeatherData)
+
+    @Query("SELECT * FROM weather_data_cache WHERE data_type = 'cache'")
+    fun getCache(): WeatherData?
+}
